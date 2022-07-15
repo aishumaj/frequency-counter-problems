4ad221d27ba5ee3a4ea254f83f2a8fad82c1e21d
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-07-15 15:39:32 -0700

    formatted document on avg pair

--- average-pair.js
+++ average-pair.js
@@ -1,7 +1,7 @@
 "use strict";
 /**Function takes two numbers and returns the average of the two numbers */
-function findAvg(a,b){
-  return (a+b)/2;
+function findAvg(a, b) {
+  return (a + b) / 2;
 }
 
 
@@ -11,16 +11,16 @@ function findAvg(a,b){
 function averagePair(nums, targetAvg) {
   //create two pointers that start at either end of sorted array
   let left = nums[0];
-  let right = nums[nums.length-1];
+  let right = nums[nums.length - 1];
 
   //take average of pointers and compare to desired avg; if avg is too high,
   //move right pointer one down; if avg is too low, move left pointer one up
-  while(left<right){
-    if(findAvg(left,right) === targetAvg){
+  while (left < right) {
+    if (findAvg(left, right) === targetAvg) {
       return true;
-    } else if(findAvg(left,right) < targetAvg){
+    } else if (findAvg(left, right) < targetAvg) {
       left++;
-    } else{
+    } else {
       right--;
     }
   }
